2a469a567112e5dc8b50d7a53e3f4df5d1ccf890
Nonty Dazana <nontyd@gmail.com>
2024-10-08 23:46:54 +0000

    create main menu

--- run.py
+++ run.py
@@ -1,2 +1,28 @@
 import json
 import os
+
+def main_menu():
+    """Displays the main menu and routes to different modules."""
+    while True:
+        print("\n--- Travel Itinerary Planner ---")
+        print("1. Manage Trips")
+        print("2. Manage Itinerary")
+        print("3. Track Expenses")
+        print("4. Manage Packing List")
+        print("5. Exit")
+
+        choice = input("Choose an option: ")
+
+        if choice == '1':
+            manage_trips_menu()
+        elif choice == '2':
+            manage_itinerary_menu()
+        elif choice == '3':
+            manage_expenses_menu()
+        elif choice == '4':
+            manage_packing_menu()
+        elif choice == '5':
+            print("Exiting the application. Goodbye!")
+            break
+        else:
+            print("Invalid option. Please choose again.")
